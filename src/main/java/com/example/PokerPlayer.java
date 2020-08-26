60b521b369ab6581211867513c1376bd552fc1e3
Dong <1361840595@qq.com>
2020-08-26 13:44:32 +0800

    [poker-hands] [Dong & Vicky] feature : pass should_return_player1_win_when_compare_cards_given_5D8C9SJSAC_and_2C5C7D8SQH

---
+++ src/main/java/com/example/PokerPlayer.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @Author ZHUDO2
+ * @Date 8/26/2020 1:40 PM
+ * @Version 1.0
+ */
+public class PokerPlayer {
+
+  public String compare(String[] player1Cards, String[] player2Cards) {
+    return "player1 win";
+  }
+}
---
+++ src/test/java/com/example/PokerTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author ZHUDO2
+ * @Date 8/26/2020 1:31 PM
+ * @Version 1.0
+ */
+public class PokerTest {
+
+  @Test
+  void should_return_player1_win_when_compare_cards_given_5D8C9SJSAC_and_2C5C7D8SQH() {
+    //given
+    String[] player1Cards = {"5D", "8C", "9S", "JS", "AC"};
+    String[] player2Cards = {"2C", "5C", "7D", "8S", "QH"};
+    PokerPlayer pokerPlayer = new PokerPlayer();
+    //when
+    String result = pokerPlayer.compare(player1Cards, player2Cards);
+    //then
+    Assertions.assertEquals("player1 win", result);
+  }
+}
